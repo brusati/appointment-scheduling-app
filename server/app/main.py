53b3556ccaa22e0e260adc1d5c8851d39d8e798b
Nico Gatti <gatti2602@gmail.com>
2023-07-27 19:04:35 -0300

    Add empty sqlite database

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,28 @@
-from typing import Union
+from fastapi import HTTPException, Depends, FastAPI
+from sqlalchemy.orm import Session
+from . import crud, models, schemas
+from .database import SessionLocal, engine
 
-from fastapi import FastAPI
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# @app.post("/users/", response_model=schemas.User)
+# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+#     db_user = crud.get_user_by_email(db, email=user.email)
+#     if db_user:
+#         raise HTTPException(status_code=400, detail="Email already registered")
+#     return crud.create_user(db=db, user=user)
 
 @app.get("/")
 def read_root():
---
+++ server/app/schemas.py
@@ -0,0 +1,4 @@
+from typing import Union
+from pydantic import BaseModel
+
+#TODO: Pydantic models here
